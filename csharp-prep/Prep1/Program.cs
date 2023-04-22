076f37db09697f02c678300a075260aa12c34274
angelicarood <leo18007@byui.edu>
2023-04-21 19:56:52 -0600

    name program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("What is your name?");
+    string name = Console.ReadLine();
+    Console.WriteLine("What is your last name?");
+    string lastname = Console.ReadLine();
+
+    Console.WriteLine($"My name is {lastname}, {name} {lastname}.");
+  }
 }
