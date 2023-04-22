385c9ae1c7028f9e18ff82b13e0172176915e3ad
angelicarood <leo18007@byui.edu>
2023-04-21 20:59:26 -0600

    prep 2 letter assigned value

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep2 World!");
+
+    Console.WriteLine("What is your grade percentage?");
+    string UserInput = Console.ReadLine();
+    int Percentage = int.Parse(UserInput);
+
+    string letter = null;
+
+    if (Percentage >= 90)
+      letter = ("A");
+
+    if (Percentage >= 80 && Percentage < 90)
+      letter = ("B");
+
+
+    if (Percentage >= 70 && Percentage < 80)
+      letter = ("C");
+
+
+    if (Percentage >= 60 && Percentage < 70)
+      letter = ("D");
+
+    if (Percentage < 60)
+      letter = ("F");
+
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.WriteLine(letter);
     }
+
+
+    if (Percentage >= 70)
+    {
+      Console.WriteLine("Congrats you passed the course");
+    }
+
+    else if (Percentage < 70)
+    {
+      Console.WriteLine("We will like to encourage you to keep working hard");
+    }
+
+  }
 }
