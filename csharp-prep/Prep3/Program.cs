8a794f035f55f7aa72b1f0e83e462cc3e1e6dfc1
angelicarood <leo18007@byui.edu>
2023-04-26 22:17:09 -0600

    low or high

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("What is the magic number?");
+    string Input = Console.ReadLine();
+    int MagicNumber;
+    Int32.TryParse(Input, out MagicNumber);
+    Console.WriteLine("What is your guess?");
+    string InputGuess = Console.ReadLine();
+    int Guess;
+    Int32.TryParse(InputGuess, out Guess);
+
+    if (MagicNumber == Guess)
+      Console.WriteLine("Your guess is right. Congrats!");
+
+
+    else if (MagicNumber > Guess)
+      Console.WriteLine("Go Higher");
+
+
+    else if (MagicNumber < Guess)
+      Console.WriteLine("Go Lower");
+
+
+  }
 }
