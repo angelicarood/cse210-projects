5b314d35a8a3b1662db6f9d603a66979b68ef8fb
angelicarood <leo18007@byui.edu>
2023-05-03 20:55:01 -0600

    Company names

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+  static void Main(string[] args)
+  {
+    Job job1 = new Job();
+
+    job1._jobTitle = "Data scientist";
+    job1._company = "Google";
+    job1.DisplayCompany();
+
+    Job job2 = new Job();
+
+    job2._company = "Facebook";
+    job2.DisplayCompany();
+  }
 }
