af37448be803bf8282c548dc3978fa3fc064cc3c
angelicarood <leo18007@byui.edu>
2023-05-17 13:04:57 -0600

    get both functions

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,34 @@
+public class Fraction
+{
+  private int _top;
+  private int _bottom;
+
+  public Fraction()
+  {
+    _top = 1;
+    _bottom = 1;
+
+  }
+  public Fraction(int wholenumber)
+  {
+    _top = wholenumber;
+    _bottom = 5;
+  }
+  public Fraction(int top, int bottom)
+  {
+    _top = top;
+    _bottom = bottom;
+  }
+  public string GetFractionString()
+  {
+    string text = $"{_top} / {_bottom}";
+    return text;
+
+  }
+
+  public int GetDecimalValue()
+  {
+    return (int)((double)_top / (double)_bottom);
+  }
+
+}
